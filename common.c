de70b9dbc138674bdc47ab79c540ad285072e70d
beromes <gomes.nunes.bernardo@gmail.com>
2022-07-02 19:34:49 -0300

    feat: adicionando funções ao common para enviar requisições e receber respostas

--- common.c
+++ common.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include "common.h"
 
+// =========================================================================================
+// Tratamento de erros
+//
+
 void dieWithUserMessage(const char *msg, const char *detail) {
     fputs(msg, stderr);
     fputs(": ", stderr);
@@ -14,3 +18,86 @@ void dieWithSystemMessage(const char *msg) {
     perror(msg);
     exit(1);
 }
+
+//
+// Fim dos métodos de tratamento de erros
+// =========================================================================================
+
+
+// =========================================================================================
+// Métodos para conversão de structs em strings e vice-versa
+//
+
+void messageToStr(Message msg, char** str) {
+    sprintf(*str, "%d|%d|%d|%s\n", msg.id, msg.originId, msg.destinationId, msg.payload);
+}
+
+void strToMessage(char* str, Message* message) {
+
+    char* ptr = strtok(str, "|");
+
+    message->id = atoi(ptr);
+
+    ptr = strtok(NULL, "|");
+    message->originId = atoi(ptr);
+
+    ptr = strtok(NULL, "|");
+    message->destinationId = atoi(ptr);
+
+    ptr = strtok(NULL, "|");
+    
+    ptr[strlen(ptr) - 1] = '\0'; // Remove '\n' que indica o fim da mensagem
+    
+    strcpy(message->payload, ptr);
+}
+
+//
+// Fim dos métodos para conversão de structs em strings e vice-versa
+// =========================================================================================
+
+
+
+// =========================================================================================
+// Métodos de comunicação com o servidor
+//
+
+// Envia mensagem para o servidor/cliente
+void sendMessage(int sock, Message msg) {
+    
+    char* strMsg = (char*) malloc(BUFFER_SIZE * sizeof(char));
+    messageToStr(msg, &strMsg);
+
+    size_t msgLen = strlen(strMsg);
+
+    ssize_t numBytes = send(sock, strMsg, msgLen, 0);
+
+    if (numBytes < 0) {
+        dieWithSystemMessage("send() failed");    
+    } else if (numBytes != msgLen) {
+        dieWithUserMessage("send()", "sent unexpected number of bytes");
+    }
+    
+    free(strMsg);
+}
+
+
+// Recebe mensagem do servidor/cliente
+void receiveMessage(int sock, Message *message) {
+
+    char strRcvd[BUFFER_SIZE]; // I/O buffer
+    memset(strRcvd, BUFFER_SIZE, 0);
+        
+    ssize_t numBytesRcvd = recv(sock, strRcvd, BUFFER_SIZE, 0);
+
+    if (numBytesRcvd < 0) {
+        dieWithSystemMessage("recv() failed");
+    } else if (numBytesRcvd == 0) {
+        dieWithUserMessage("recv()", "connection closed prematurely");
+    }
+
+    strToMessage(strRcvd, message);
+}
+
+//
+// Fim dos métodos de comunicação com o servidor
+// =========================================================================================
--- common.h
+++ common.h
@@ -1,10 +1,44 @@
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #ifndef COMMON_H
 #define COMMON_H
 
 #define BUFFER_SIZE 500
+#define DELIMETER_END_OF_MESSAGE '\n'
+#define DELIMTER_MESSAGE_SECTION '|'
 
+// IDs das mensagens
+#define REQ_ADD  1
+#define REQ_REM  2
+#define RES_ADD  3
+#define RES_LIST 4
+#define REQ_INF  5
+#define RES_INF  6
+#define ERROR    7
+#define OK       8
+
+typedef struct Message {
+    int id;
+    int originId;
+    int destinationId;
+    char payload[100];
+} Message;
+
+// Mensagens de erro
 void dieWithUserMessage(const char *msg, const char *detail);
-
 void dieWithSystemMessage(const char *msg);
+
+// Conversão de mensagens em strings
+void messageToStr(Message msg, char** str);
+void strToMessage(char* str, Message* message);
+
+// Envio/Recebimento de mensagens
+void sendMessage(int sock, Message msg);
+void receiveMessage(int sock, Message* msg);
 
 #endif
